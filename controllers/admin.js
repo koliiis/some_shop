62068997a3e0d7864968ceca2cac1acaf1a42801
koliiis <123481146+koliiis@users.noreply.github.com>
2025-02-03 20:27:30 +0200

    improved-storing-product-data

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,15 +3,16 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product', {
       pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
+      path: '/admin/add-product'
     });
   };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, price, description);
     product.save();
     res.redirect('/');
 };
