3df8e483fe5166a1bc1502e8b088b8ba346396b9
koliiis <123481146+koliiis@users.noreply.github.com>
2025-02-10 13:30:28 +0200

    updated-connection

--- util/database.js
+++ util/database.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'node-complete'
+    database: 'node-complete',
+    password: 'Homaterminator27'
 });
 
 module.exports = pool.promise();
